ce178b746822d35a9f3bf6eef2acfb65d7af9a3c
Nguyễn Thái Dương <loswkpq17@gmail.com>
2021-06-17 02:46:56 +0700

    init project

---
+++ src/main/java/com/g18/SlaServerApplication.java
@@ -0,0 +1,13 @@
+package com.g18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g18/SlaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
