9a95f7e47d24e470cfa2cb2c87287e14f12f5432
Nguyễn Thái Dương <loswkpq17@gmail.com>
2021-06-18 17:26:16 +0700

    asyns processing send mail

--- src/main/java/com/g18/SlaServerApplication.java
+++ src/main/java/com/g18/SlaServerApplication.java
@@ -2,8 +2,10 @@ package com.g18;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SlaServerApplication {
 
 	public static void main(String[] args) {
