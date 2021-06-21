4ef7165a96307cf4c27953a583ba219f450e089f
Nguyễn Thái Dương <loswkpq17@gmail.com>
2021-06-21 18:26:20 +0700

    fixed null field request

--- src/main/java/com/g18/dto/LoginRequest.java
+++ src/main/java/com/g18/dto/LoginRequest.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -13,8 +15,12 @@ import javax.validation.constraints.Size;
 public class LoginRequest {
     @Pattern(regexp="^(?=.{2,20}$)[a-zA-Z0-9_][a-zA-Z0-9_.]*",
             message = "Usernames can only use letters, numbers, underscores and periods.")
+    @NotBlank(message = "Username is required")
+    @NotNull(message = "Username is required")
     private String username;
 
     @Size(min = 5, max = 20, message = "Password must be between 5 and 20 characters")
+    @NotBlank(message = "Password is required")
+    @NotNull(message = "Password is required")
     private String password;
 }
