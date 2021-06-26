7f5439773048972a391e15420e6779e39472b129
hoangnn-4310 <hoanghero220599@Gmail.com>
2021-06-26 22:35:56 +0700

    commit controller, repository StudySet

---
+++ src/main/java/com/g18/controller/StudySetController.java
@@ -0,0 +1,55 @@
+package com.g18.controller;
+
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.g18.service.StudySetService;
+
+@RestController
+@RequestMapping("api/studySet")
+@AllArgsConstructor
+public class StudySetController {
+    
+	@Autowired
+    private StudySetService studySetService;
+	
+	@GetMapping("/list")
+    public List<ObjectNode> listStudySet(@RequestBody ObjectNode json){
+        return studySetService.listStudySet(json);
+    }
+	
+	@GetMapping("/view/{id}")
+    public ObjectNode  viewStudySet(@PathVariable Long id){
+        return studySetService.viewStudySetBy(id);
+    }
+	
+	@PostMapping("/create")
+	public String createRoom(@RequestBody ObjectNode json){ 
+		return studySetService.createStudySet(json);
+	}
+	
+	@PutMapping("/edit")
+	public String editStudySet(@RequestBody ObjectNode json){
+		return studySetService.editStudySet(json);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String deleteStudySet(@PathVariable Long id) {
+		return studySetService.deleteStudySet(id);
+	}
+	
+	
+	
+	
+
+	    
+
+}
+
---
+++ src/main/java/com/g18/repository/StudySetRepository.java
@@ -0,0 +1,9 @@
+package com.g18.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.g18.entity.StudySet;
+
+public interface StudySetRepository extends JpaRepository<StudySet, Long>{
+
+}
