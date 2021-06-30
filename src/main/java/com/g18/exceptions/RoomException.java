cce717760f41f81a894a335a9108aed7a9779764
khanhngocc <khanhnnhe130816@fpt.edu.vn>
2021-06-30 12:06:22 +0700

    'remove no use [RoomExceptionClass]'

--- src/main/java/com/g18/exceptions/RoomException.java
+++
@@ -1,7 +0,0 @@
-package com.g18.exceptions;
-
-public class RoomException extends RuntimeException{
-    public RoomException(String exMessage, Exception exception) { super(exMessage, exception); }
-
-    public RoomException(String exMessage) { super(exMessage); }
-}
