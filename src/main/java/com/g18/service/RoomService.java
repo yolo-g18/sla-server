924a9a491bdc5c9ffa64e0594b8ad3b9e5a48fe2
khanhngocc <khanhnnhe130816@fpt.edu.vn>
2021-07-01 22:19:36 +0700

    'modify [verify user is member]'

--- src/main/java/com/g18/service/RoomService.java
+++ src/main/java/com/g18/service/RoomService.java
@@ -569,13 +569,13 @@ public class RoomService {
         // get creator of room
         User creatorOfRoom = existingRoom.getOwner();
 
-        // verify user is creator of room (creator also a member of room)
+        // firstly, verify user is creator of room (creator also a member of room)
         if(currenUserLogined.getId().equals(creatorOfRoom.getId()))
         {
             return true;
         }
 
-        // determine user is member
+        // secondly, determine user is member
         RoomMember roomMember = existingRoom.getRoomMembers().stream().filter(
                 member -> member.getRoomMemberId().getMemberId().equals(currenUserLogined.getId())
         ).findAny().orElse(null);
