96708578d900b04e86b18ceea016c34513a53932
khanhngocc <khanhnnhe130816@fpt.edu.vn>
2021-07-01 22:21:57 +0700

    'modify [permisson for add folder to Room]'

--- src/main/java/com/g18/service/RoomService.java
+++ src/main/java/com/g18/service/RoomService.java
@@ -271,7 +271,7 @@ public class RoomService {
         }
 
         // verify room's permisson
-        if(isCreatorOfRoom(room_id) == false || isMemberOfRoom(room_id) == false)
+        if(isMemberOfRoom(room_id) == false)
             return "You are not member of Room, You don't have permisson!!!";
 
         // parsing id of folder
