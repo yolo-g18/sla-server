f0e77dc8be432f1cf70e2d39e0db32fcbd2cc3e2
khanhngocc <khanhnnhe130816@fpt.edu.vn>
2021-07-04 14:55:04 +0700

    'create [edit Folder controller]'

--- src/main/java/com/g18/controller/FolderController.java
+++ src/main/java/com/g18/controller/FolderController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.g18.service.FolderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public class FolderController {
@@ -14,5 +15,8 @@ public class FolderController {
     @PostMapping("/createFolder")
     public String createFolder(@RequestBody ObjectNode json){ return folderService.saveFolder(json);}
 
-
+    @PutMapping("/editFolder")
+    public String editFolder(@RequestBody ObjectNode json){
+        return folderService.editFolder(json)
+    }
 }
