33592c62ff92c45c45db6f952776600214559081
khanhngocc <khanhnnhe130816@fpt.edu.vn>
2021-07-04 14:43:39 +0700

    'create [save Folder controller]'

--- src/main/java/com/g18/controller/FolderController.java
+++ src/main/java/com/g18/controller/FolderController.java
@@ -1,5 +1,18 @@
 package com.g18.controller;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.g18.service.FolderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public class FolderController {
+
+    @Autowired
+    private FolderService folderService;
+
+    @PostMapping("/createFolder")
+    public String createFolder(@RequestBody ObjectNode json){ return folderService.saveFolder(json);}
+
 
 }
