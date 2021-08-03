4788822b865b12ae695aea65254803629f3be2d6
vinhnc28071999 <56397566+congvinh287@users.noreply.github.com>
2021-08-03 10:34:37 +0700

    fix add event null bug

--- src/main/java/com/g18/service/IS/IEventService.java
+++ src/main/java/com/g18/service/IS/IEventService.java
@@ -1,11 +1,12 @@
 package com.g18.service.IS;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.g18.dto.EventDto;
 
 import java.util.List;
 
 public interface IEventService {
-    String save(EventDto eventDto);
+    String save(ObjectNode json);
     EventDto update(EventDto eventDto);
     void delete(long[] ids);
     List<EventDto> getAllBetweenDates(String from, String to);
